d631266e7d5bbcfcfc37df5a183cd69a03f8101b
Rurik Bugdanov <Rurik Bugdanov>
2017-04-11 00:50:04 +0700

    added LICENSE

--- index.js
+++ index.js
@@ -1,2 +1,35 @@
+/*
+ * index.js
+ *
+ * Copyright (c) 2016-2017 ALSENET SA - http://www.alsenet.com
+ *
+ * Author(s):
+ *
+ *      Rurik Bogdanov <rurik.bugdanov@alsenet.com>
+ *
+ * This file is part of the electron-dataminer project at:
+ *
+ *      <http://github.com/alsenet-labs/electron-dataminer>.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Additional Terms:
+ *
+ *      You are required to preserve legal notices and author attributions in
+ *      that material or in the Appropriate Legal Notices displayed by works
+ *      containing it.
+ *
+ */
 
 var edm=require('electron-dataminer');
